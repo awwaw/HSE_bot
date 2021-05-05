d3c309a8b78837634a044777cd4b85ef6ef20df4
Данила Суворов <dansuvtim@gmail.com>
2021-05-05 17:27:46 +0300

    token

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from bot import Bot
-<<<<<<<<< Temporary merge branch 1
 from EchoSkill import EchoSkill
-=========
-from skill import EchoSkill
-from ELIZA import ELIZA
->>>>>>>>> Temporary merge branch 2
-
 
 def main():
     bot = Bot('TOKEN')
