f04959894d2627977e1a9f8347e3155f475432b2
awwaw <vovapodkorytov966@gmail.com>
2021-05-05 16:15:46 +0300

    aboba

--- bot.py
+++ bot.py
@@ -1,18 +1,22 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filters
+from skill import EchoSkill
 
 
-class Bot():
+class Bot:
     def __init__(self, token):
         self.token = token
         self.updater = Updater(token=self.token, use_context=True)
         self.dispatcher = self.updater.dispatcher
         self.message_handler = MessageHandler(Filters.text & (~Filters.command), self.get_message)
         self.dispatcher.add_handler(self.message_handler)
+        self.skills = []
 
     def get_message(self, update, context):
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text='aboba')
+        message = update.message.text
+        skill = EchoSkill(message, update, context)
+        if skill.match():
+            skill.answer()
 
     def run(self):
         self.updater.start_polling()
--- skill.py
+++ skill.py
@@ -1,12 +1,12 @@
-class EchoSkill():
+class EchoSkill:
     def __init__(self, message, update, context):
         self.message = message
         self.keywords = []
         self.update = update
         self.context = context
 
-    def match(self, message):
-        return True
+    def match(self):
+        return bool(self.message)
 
     def answer(self):
         self.context.bot.send_message(chat_id=self.update.effective_chat.id,
