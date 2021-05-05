488c93d133348686501362159791cb26ab4749af
Alisheva Azalia <azalia.alischeva@yandex.ru>
2021-05-05 18:03:19 +0300

    formatted code

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from telegram.ext import Updater, CallbackContext
 from telegram import Update
@@ -15,37 +14,40 @@ class Bot():
         self.dispatcher.add_handler(self.message_handler)
         self.skills = [EchoSkill()]
 
-        self.message = self.get_message
-        self.words = self.split_to_tokens()
-        self.dict = {"were": "was", "'m": "i am", "'re": "you are"}
+        self.dict = {"were": "was", "'m": "am", "'re": "are"}
 
-    def split_to_sent(self):
-        tokenized_text = sent_tokenize(self.message)
+    def split_to_sent(self, sentence: str):
+        tokenized_text = sent_tokenize(sentence)
         return tokenized_text
 
-    def split_to_tokens(self):
-        tokenized_text = self.split_to_sent()
+    def split_to_tokens(self, sentences: str):
+        tokenized_text = self.split_to_sent(sentences)
         words = []
         for sent in tokenized_text:
             words.append(word_tokenize(sent))
         return words
 
-    def process(self):
-        for i in range(len(self.words)):
-            for j in range(len(self.words[i])):
-                new_word = self.words[i][j].lower()
+    def preprocess(self, sentences: str):
+        words = self.split_to_tokens(sentences)
+        for i in range(len(words)):
+            for j in range(len(words[i])):
+                new_word = words[i][j].lower()
                 if new_word in self.dict:
                     new_word = self.dict[new_word]
-                self.words[i][j] = new_word
+                words[i][j] = new_word
+        return words
 
     def get_message(self, update: Update, context: CallbackContext):
         message = update.message.text
-        for skill in self.skills:
-            if skill.match(message):
-                context.bot.send_message(chat_id=update.effective_chat.id,
-                                         text=skill.answer(message))
-                break
+
+        answer = str(self.preprocess(message))
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text=answer)
+        # for skill in self.skills:
+        #     if skill.match(message):
+        #         context.bot.send_message(chat_id=update.effective_chat.id,
+        #                                  text=skill.answer(message))
+        #         break
 
     def run(self):
         self.updater.start_polling()
-
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from bot import Bot
+import nltk
 from EchoSkill import EchoSkill
 
 
 def main():
-    bot = Bot('TOKEN')
+    nltk.download('punkt')
+    bot = Bot("1665573427:AAEuFh15FLJfoAqGrAXuE6xY30wmpcGu3qY")
     bot.run()
 
 
