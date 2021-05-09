a58f01a5f747a9f1d56853d2bc1df8fe7a45e0c3
awwaw <vovapodkorytov966@gmail.com>
2021-05-09 19:30:23 +0300

    all

--- BERT.py
+++ BERT.py
@@ -1,6 +1,9 @@
 from transformers import BertModel, BertTokenizer, BertConfig
 import numpy as np
 from datetime import datetime as dt
+import torch
+from torch.utils.data import TensorDataset, DataLoader
+
 
 
 # model = BertModel.from_pretrained('DeepPavlov/rubert-base-cased-conversational')
@@ -9,42 +12,61 @@ from datetime import datetime as dt
 # data = tokenizer.encode_plus('Февраль! Достать чернил и плакать', return_tensors='pt')
 # res = model(**data)
 # print(res)
+#
+# start = dt.now()
+# print(start)
+#
+# requests_file = open("static/documents/requests.txt", "r", encoding="utf-8")
+# responses_file = open("static/documents/responses.txt", "r", encoding="utf-8")
+#
+# requests = []
+# responses = []
+# target = []
+#
+# for line in requests_file.readlines():
+#     requests.append(line[:-1])
+#
+# for line in responses_file.readlines():
+#     responses.append(line[:-1])
+#
+# target = [1] * len(responses)
+#
+# print(dt.now())
+#
+# c_req = requests
+# for i in range(5):
+#     req = np.array(c_req)
+#     req = np.random.permutation(req)
+#     requests += list(req)
+#     responses += responses
+#     target += [0] * len(c_req)
+#
+# print(dt.now())
+#
+# req_output = open("requests_db.txt", "w", encoding='utf-8')
+# resp_output = open("responses_db.txt", "w", encoding='utf-8')
+# req_output.writelines('\n'.join(requests))
+# resp_output.writelines('\n'.join(responses))
+#
+# end = dt.now()
+# print(end - start)
 
-start = dt.now()
-print(start)
 
-requests_file = open("static/documents/requests.txt", "r", encoding="utf-8")
-responses_file = open("static/documents/responses.txt", "r", encoding="utf-8")
+model = BertModel.from_pretrained('DeepPavlov/rubert-base-cased-conversational')
+tokenizer = BertTokenizer.from_pretrained('DeepPavlov/rubert-base-cased-conversational')
 
-requests = []
-responses = []
-target = []
+data, labels = torch.load('static/documents/train_ids.torch')
 
-for line in requests_file.readlines():
-    requests.append(line[:-1])
+dataset = TensorDataset(data.input_ids, data.token_type_ids, data.attention_mask)
+loader = DataLoader(dataset, shuffle=False, batch_size=4)
 
-for line in responses_file.readlines():
-    responses.append(line[:-1])
+result = torch.LongTensor()
+iterations = 0
+for a, b, c in loader:
+    res = model(input_ids=a, token_type_ids=b, attention_mask=c)
+    result = torch.cat((result, res.pooler_output))
+    iterations += 1
+    if iterations == 100:
+        break
 
-target = [1] * len(responses)
-
-print(dt.now())
-
-c_req = requests
-for i in range(5):
-    req = np.array(c_req)
-    req = np.random.permutation(req)
-    requests += list(req)
-    responses += responses
-    target += [0] * len(c_req)
-
-print(dt.now())
-
-req_output = open("requests_db.txt", "w", encoding='utf-8')
-resp_output = open("responses_db.txt", "w", encoding='utf-8')
-req_output.writelines('\n'.join(requests))
-resp_output.writelines('\n'.join(responses))
-
-end = dt.now()
-print(end - start)
-
+print(result)
