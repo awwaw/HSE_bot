2a64628ba9b81fc4c17138185b86ee639c2c6ee1
awwaw <vovapodkorytov966@gmail.com>
2021-05-09 21:21:13 +0300

    Added DialogueSkill based on neural network

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ from bot.skills.echo import EchoSkill
 from bot.skills.pushkin import PushkinSkill
 from bot.skills.dialogue import DialogueSkill
 
-from datetime import datetime as dt
 
 def main():
     bot = Bot(sys.argv[1], [
         MathSkill(),
-        PushkinSkill('static/documents/save_state.tmp'),
+        PushkinSkill('save_state.tmp'),
         DialogueSkill(),
         ElizaSkill('good_doctor.txt'),
         EchoSkill()
@@ -22,12 +21,5 @@ def main():
 
 
 if __name__ == '__main__':
-    # nltk.download('punkt')
-    rf = Classifier(max_depth=3)
-    rf.prepare_data('static/documents/unique_dialogues.txt')
-
-    start = dt.now()
-    rf.test()
-    end = dt.now()
-    print(end - start)
+    nltk.download('punkt')
     main()
